705b5f08186e15be9d60caaba429176d98602a32
Joel Aliyu <joelaliyu1@gmail.com>
2025-11-12 12:01:17 +0000

    Initialized project with shadcn and tailwindcss

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Button className="cursor-pointer">Click Me!!!</Button>
+    </div>
+  );
+}
